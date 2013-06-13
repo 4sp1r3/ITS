c6f678679d3645c9b598b6bbebca0439531c077b
Matias Parodi - ENS <mparodilabs@gmail.com>
2013-06-13 15:07:18 -0300

    Remove NB project

--- malva/examples/its/newGA/newGA.req.cc
+++ malva/examples/its/newGA/newGA.req.cc
@@ -1,13 +1,6 @@
 #ifndef INC_REQ_newGA
 #define INC_REQ_newGA
 
-#include "newGA.hh"
-#include "problem.cc"
-#include "solution.cc"
-#include "stats.cc"
-#include "operators.cc"
-#include "stop.cc"
-
 #include <math.h>
 
 skeleton newGA {
